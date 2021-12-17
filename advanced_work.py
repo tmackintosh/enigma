029929745aca19f154b265175511a83b28668677
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2021-12-17 15:50:06 +0000

    Find fittest ring settings

--- advanced_work.py
+++ advanced_work.py
@@ -10,7 +10,6 @@ from enigma import EnigmaMachine
 code = "OZLUDYAKMGMXVFVARPMJIKVWPMBVWMOIDHYPLAYUWGBZFAFAFUQFZQISLEZMYPVBRDDLAGIHIFUJDFADORQOOMIZPYXDCBPWDSSNUSYZTJEWZPWFBWBMIEQXRFASZLOPPZRJKJSPPSTXKPUWYSKNMZZLHJDXJMMMDFODIHUBVCXMNICNYQBNQODFQLOGPZYXRJMTLMRKQAUQJPADHDZPFIKTQBFXAYMVSZPKXIQLOQCVRPKOBZSXIUBAAJBRSNAFDMLLBVSYXISFXQZKQJRIQHOSHVYJXIFUZRMXWJVWHCCYHCXYGRKMKBPWRDBXXRGABQBZRJDVHFPJZUSEBHWAEOGEUQFZEEBDCWNDHIAQDMHKPRVYHQGRDYQIOEOLUBGBSNXWPZCHLDZQBWBEWOCQDBAFGUVHNGCIKXEIZGIZHPJFCTMNNNAUXEVWTWACHOLOLSLTMDRZJZEVKKSSGUUTHVXXODSKTFGRUEIIXVWQYUIPIDBFPGLBYXZTCOQBCAHJYNSGDYLREYBRAKXGKQKWJEKWGAPTHGOMXJDSQKYHMFGOLXBSKVLGNZOAXGVTGXUIVFTGKPJU"
 rotors = ["I", "II", "III", "IV", "V"]
 
-
 # possible_config = find_rotor_configuration(code, rotors)
 possible_config = [
     ['IV II V', 0.026243182184691613, 'M D Q'], 
@@ -20,19 +19,32 @@ possible_config = [
     ['II V III', 0.02620213308703874, 'M V L']  ]
 
 top_rotors = get_best_rotors(possible_config)
-possible_config = find_rotor_configuration(code, top_rotors)
+# possible_config = find_rotor_configuration(code, top_rotors)
+possible_config = [
+    ['II V III', 0.026219725557461403, 'M V L'], 
+    ['II III V', 0.026190404773423638, 'H R D'], 
+    ['II III V', 0.02613176320534811, 'L S I'], 
+    ['III II V', 0.026114170734925446, 'K R N'], 
+    ['II V III', 0.02610830657811789, 'J H W'] ]
 
-for rotor_config in top_rotors:
-    print(possible_config)
+best_settings = []
 
-    for enigma_state in possible_config:
-        machine = EnigmaMachine(get_string_form(rotor_config), "A", initial_positions = enigma_state[2])
+def fitness_value(a):
+    return a[3]
 
-        optimal_rotor_setting = optimise_rotor_setting(machine, 1, code)[0]
-        machine.starting_rotor.setting = optimal_rotor_setting[1]
+for enigma_state in possible_config:
+    machine = EnigmaMachine(enigma_state[0], "A", initial_positions = enigma_state[2])
 
-        optimal_rotor_setting = optimise_rotor_setting(machine, 2, code)[0]
-        machine.starting_rotor.left_connection.setting = optimal_rotor_setting[1]
+    optimal_rotor_setting_1 = optimise_rotor_setting(machine, 1, code)[0]
+    machine.starting_rotor.setting = optimal_rotor_setting_1[1]
 
-        encoded = machine.encode(code)
-        print(bigram_fitness(encoded))
+    optimal_rotor_setting_2 = optimise_rotor_setting(machine, 2, code)[0]
+    machine.starting_rotor.left_connection.setting = optimal_rotor_setting_2[1]
+
+    encoded = machine.encode(code)
+    fitness = bigram_fitness(encoded)
+    state = [enigma_state[0], enigma_state[2], [optimal_rotor_setting_1, optimal_rotor_setting_2], fitness]
+    best_settings.append(state)
+
+best_settings.sort(key = fitness_value)
+print(best_settings)
